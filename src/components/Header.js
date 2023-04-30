b09fa6d578e827a3a859dd48ece42ef7209cf245
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-30 18:51:03 +0200

    Handle different slices to set user authentication status and display content accordingly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,32 @@
 import classes from './Header.module.css';
+import {  useDispatch } from 'react-redux';
+import { authActions } from '../store';
 
-const Header = () => {
+const Header = props => {
+  const dispatch = useDispatch();
+  const isLoggedIn = props.isAuth;
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {isLoggedIn &&
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button type="button" onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        }
       </nav>
     </header>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: false }
+const initialCounterState = { counter: 0, showCounter: false, isAuthenticated: false }
+const initialAuthState = { isAuthenticated: false }
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: {
         increment(state) {
             state.counter++;
@@ -24,10 +25,28 @@ const counterSlice = createSlice({
     }
 });
 
+const authSlice = createSlice({
+    name: 'athentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
 const store = configureStore({
-    reducer:  counterSlice.reducer,
+    reducer:  {
+        counter: counterSlice.reducer,
+        authentication: authSlice.reducer,
+    }
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
